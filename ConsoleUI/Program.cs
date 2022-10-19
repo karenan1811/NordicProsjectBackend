417f7272594a345bc89b9531be67931533b16a11
karenan1811 <89387334+karenan1811@users.noreply.github.com>
2022-10-19 14:19:16 +0200

    new

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using SuggestionApp.DatabaseConnection;
+
+EmployeeDbContext employeeDbContext = new EmployeeDbContext();
+
+Console.WriteLine("--------------------------Teams Information From Database----------------------------");
+var teams = employeeDbContext.Teams.ToList();
+foreach(var team in teams)
+{
+    Console.WriteLine(" Team Leader: "+ team.TeamLeader + " Team Name:" + team.TeamName);
+}
+Console.WriteLine("----------------Employees Information From Database---------------------");
+var employees = employeeDbContext.Employees.ToList();
+foreach(var employee in employees)
+{
+    Console.WriteLine("Employee Details= Employee username :"+employee.Username +"Employee Name"+ employee.FirstName + " "+employee.LastName);
+}
+Console.WriteLine("Suggestions Information From Database");
+var suggestions = employeeDbContext.Suggestions.ToList();
+foreach (var suggestion in suggestions)
+{
+    Console.WriteLine(suggestion.PriorityLevel);
+}
